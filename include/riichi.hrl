4524e2227b077ff9638061ff7a73e99bc53fd58d
Correl Roush <correl@gmail.com>
2012-08-01 00:11:36 -0400

    Improved header type documentation

--- include/riichi.hrl
+++ include/riichi.hrl
@@ -5,10 +5,16 @@
 -define(HONOURS, ?DRAGONS ++ ?WINDS).
 -define(TILES, ?SIMPLES ++ ?TERMINALS ++ ?HONOURS).
 
+%% @type wind() = east | south | west | north
 -type wind()   :: east | south | west | north.
 
+%% @type dragon() = green | red | white
 -type dragon() :: green | red | white.
 
+%% @type tile() = {tile, Suit, Value, From}
+%%     Suit  = pin | man | sou | wind | dragon
+%%     Value = integer() | wind() | dragon()
+%%     From  = draw | wind()
 -record(tile, {
           suit       :: pin | man | sou | wind | dragon,
           value      :: integer() | wind() | dragon(),
@@ -16,18 +22,30 @@
 }).
 -type tile() :: #tile{}.
 
+%% @type meld() = {meld, Type, Tiles}
+%%     Type = pair | chii | pon | kan
+%%     Tiels = [tile()]
 -record(meld, {
           type  :: pair | chii | pon | kan,
           tiles :: [tile()]
 }).
 -type meld() :: #meld{}.
 
+%% @type hand() = {hand, Tiles, Melds}
+%%     Tiles = [tile()]
+%%     Melds = [meld()]
 -record(hand, {
           tiles=[] :: [tile()],
           melds=[] :: [meld()]
 }).
 -type hand() :: #hand{}.
 
+%% @type player() = {player, Name, Seat, Hand, Discards, Drawn}
+%%     Name = string()
+%%     Seat = wind()
+%%     Hand = hand()
+%%     Discards = [tile()]
+%%     Drawn = none | {tsumo | ron, tile()}
 -record(player, {
           name     :: string(),
           seat     :: wind(),
@@ -37,8 +55,21 @@
 }).
 -type player() :: #player{}.
 
+%% @type phase() = Phase
+%%     Phase = draw | discard
 -type phase() :: draw | discard.
 
+%% @type game() = {game, Rounds, Timeout, Round, Turn, Phase, Wall, Rinshan, Dora, Uradora, Players}
+%%     Rounds = integer()
+%%     Timeout = integer() | infinity
+%%     Round = wind()
+%%     Turn = wind()
+%%     Phase = phase()
+%%     Wall = [tile()]
+%%     Rinshan = [tile()]
+%%     Dora = [tile()]
+%%     Uradora = [tile()]
+%%     Players = [player()]
 -record(game, {
           rounds=2         :: integer(),
           timeout=infinity :: integer() | infinity,
