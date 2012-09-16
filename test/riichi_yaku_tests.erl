0c58354f95eaee472d85fb38f20ece2e6a11d1e2
Correl Roush <correl@gmail.com>
2012-09-16 15:06:15 -0400

    More yaku

--- test/riichi_yaku_tests.erl
+++ test/riichi_yaku_tests.erl
@@ -44,6 +44,14 @@ iipeikou_test() ->
                         #meld{type=chii, tiles=[#tile{suit=man, value=V} || V <- [6,7,8]]}]},
     ?assertEqual(true, yaku:iipeikou(#game{}, #player{hand=Hand, drawn={tsumo, #tile{suit=sou, value=6}}})).
 
+ryanpeikou_not_iipeikou_test() ->
+    Hand = #hand{melds=[#meld{type=pair, tiles=lists:duplicate(2, #tile{suit=sou, value=8})},
+                        #meld{type=chii, tiles=[#tile{suit=pin, value=V} || V <- [1,2,3]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=chii, tiles=[#tile{suit=pin, value=V} || V <- [1,2,3]]}]},
+    ?assertEqual(false, yaku:iipeikou(#game{}, #player{hand=Hand, drawn={tsumo, #tile{suit=sou, value=6}}})).
+
 chanta_test() ->
     Hand = #hand{melds=[#meld{type=pair, tiles=lists:duplicate(2, #tile{suit=dragon, value=red})},
                         #meld{type=chii, tiles=[#tile{suit=pin, value=V} || V <- [1,2,3]]},
@@ -100,9 +108,57 @@ san_an_kou_test() ->
                         #meld{type=chii, tiles=[#tile{suit=man, value=V} || V <- [6,7,8]]}]},
     ?assertEqual(true, yaku:san_an_kou(#game{}, #player{hand=Hand, drawn={tsumo, #tile{suit=dragon, value=red}}})).
 
+shou_san_gen_test() ->
+    Hand = #hand{melds=[#meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [2,3,4]]},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=dragon, value=red})},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=dragon, value=green})},
+                        #meld{type=kan, tiles=lists:duplicate(4, #tile{suit=pin, value=1})},
+                        #meld{type=pair, tiles=lists:duplicate(2, #tile{suit=dragon, value=white})}]},
+    ?assert(yaku:shou_san_gen(#game{}, #player{hand=Hand})).
+
+honrouto_test() ->
+    Hand = #hand{melds=[#meld{type=pon, tiles=lists:duplicate(3, #tile{suit=sou, value=1})},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=sou, value=9})},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=dragon, value=green})},
+                        #meld{type=kan, tiles=lists:duplicate(4, #tile{suit=pin, value=1})},
+                        #meld{type=pair, tiles=lists:duplicate(2, #tile{suit=dragon, value=white})}]},
+    ?assert(yaku:honrouto(#game{}, #player{hand=Hand})).    
+
 chiitoitsu_test() ->
     Hand = #hand{melds=[#meld{type=pair, tiles=lists:duplicate(2, #tile{suit=pin, value=V})} || V <- lists:seq(1,7)]},
     ?assertEqual(true, yaku:chiitoitsu(#game{}, #player{hand=Hand})).
+
+honitsu_test() ->
+    Hand = #hand{melds=[#meld{type=pair, tiles=lists:duplicate(2, #tile{suit=sou, value=8})},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=dragon, value=red})},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=sou, value=1})}]},
+    ?assertEqual(true, yaku:honitsu(#game{}, #player{hand=Hand, drawn={tsumo, #tile{suit=sou, value=1}}})).
+
+jun_chan_test() ->
+    Hand = #hand{melds=[#meld{type=pair, tiles=lists:duplicate(2, #tile{suit=sou, value=9})},
+                        #meld{type=chii, tiles=[#tile{suit=pin, value=V} || V <- [1,2,3]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [7,8,9]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [7,8,9]]},
+                        #meld{type=chii, tiles=[#tile{suit=pin, value=V} || V <- [1,2,3]]}]},
+    ?assertEqual(true, yaku:jun_chan(#game{}, #player{hand=Hand, drawn={tsumo, #tile{suit=sou, value=7}}})).
+
+ryanpeikou_test() ->
+    Hand = #hand{melds=[#meld{type=pair, tiles=lists:duplicate(2, #tile{suit=sou, value=8})},
+                        #meld{type=chii, tiles=[#tile{suit=pin, value=V} || V <- [1,2,3]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=chii, tiles=[#tile{suit=pin, value=V} || V <- [1,2,3]]}]},
+    ?assertEqual(true, yaku:ryanpeikou(#game{}, #player{hand=Hand, drawn={tsumo, #tile{suit=sou, value=6}}})).
+
+chinitsu_test() ->
+    Hand = #hand{melds=[#meld{type=pair, tiles=lists:duplicate(2, #tile{suit=sou, value=8})},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=sou, value=9})},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=chii, tiles=[#tile{suit=sou, value=V} || V <- [4,5,6]]},
+                        #meld{type=pon, tiles=lists:duplicate(3, #tile{suit=sou, value=1})}]},
+    ?assertEqual(true, yaku:chinitsu(#game{}, #player{hand=Hand, drawn={tsumo, #tile{suit=sou, value=1}}})).
 
 kokushi_musou_test() ->
     Hand = #hand{tiles=?TERMINALS ++ ?HONOURS -- [#tile{suit=pin, value=1}],
