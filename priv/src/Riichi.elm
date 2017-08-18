be02a1fa1266f9c2d8083db3227534d7e59eb670
Correl Roush <correl@gmail.com>
2017-08-17 23:31:24 -0400

    Add tileset toggle

--- priv/src/Riichi.elm
+++ priv/src/Riichi.elm
@@ -2,6 +2,8 @@ module Riichi exposing (..)
 
 import Hand
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Html.CssHelpers
 import Stylesheets as S
 
@@ -11,16 +13,19 @@ import Stylesheets as S
 
 
 type alias Model =
-    { hand : Hand.Model }
+    { tileset : S.Tileset
+    , hand : Hand.Model
+    }
 
 
 type Msg
-    = Noop
+    = SetTileset S.Tileset
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { hand =
+    ( { tileset = S.White
+      , hand =
             Hand.fromJSON
                 { tiles =
                     [ "4 pin"
@@ -42,7 +47,11 @@ init =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SetTileset tileset ->
+            ( { model | tileset = tileset }
+            , Cmd.none
+            )
 
 
 main : Program Never Model Msg
@@ -57,7 +66,26 @@ main =
 
 view : Model -> Html Msg
 view model =
-    div [ class [ S.Tileset S.White ] ]
+    div [ class [ S.Tileset model.tileset ] ]
         [ h1 [] [ text "Riichi Mahjong" ]
+        , fieldset []
+            [ legend [] [ text "Tile Set" ]
+            , radio "tileset" "White" (SetTileset S.White) (model.tileset == S.White)
+            , radio "tileset" "Black" (SetTileset S.Black) (model.tileset == S.Black)
+            ]
         , Hand.view model.hand
+        ]
+
+
+radio : String -> String -> Msg -> Bool -> Html Msg
+radio name_ value msg checked_ =
+    label []
+        [ input
+            [ type_ "radio"
+            , name name_
+            , onClick msg
+            , checked checked_
+            ]
+            []
+        , text value
         ]
